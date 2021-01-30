6b4be42952be22f0d761ee4360a32f0dbce78997
Leon0241 <enliang.leon.lee@gmail.com>
2021-01-30 03:37:45 +0000

    array stuff

--- Data/textConversion.py
+++ Data/textConversion.py
@@ -1,15 +1,34 @@
-file = open("data/unfilteredText.txt", "r", encoding = 'utf-8')
-contents = file.read()
-file.close()
-contents = contents.split("\n")
-formattedContents = [[0, 0, 0]]
+def filestuff():
+   file = open("data/testdoc.txt", "r", encoding = 'utf-8')
+   contents = file.read()
+   file.close()
+   contents = contents.split("\n")
+   return contents
 
+def arraystuff():
+   formattedContents = [[0, 0, 0]]
+   date = ""
 
-state = [True]
-date = ""
+   for i in range(len(contents)):
+      array = [0] * 3
+      string = contents[i]
+      header = False
 
-array = [0] * 3
-for i in range(len(contents)):
-   if contents[i].rfind("leon02four") == 0:
-      print(contents[i])
-   elif contents[i].rfind("みどり") == 0:
+      if string.rfind("leon02four") == 0:
+         state = True
+         header = True
+         date = string[-10:]
+      elif string.rfind("みどり") == 0:
+         state = False
+         header = True
+      if state == True and header == False:
+         array[0] = "Leon"
+         array[1] = date
+         array[2] = string
+         formattedContents.append(array)
+   return formattedContents
+
+contents = filestuff()
+state = True
+formatted = arraystuff()
+print(formatted)
