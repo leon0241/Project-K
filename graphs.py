3913cf55d3434036e40dee944a10fab4d889265d
Leon0241 <enliang.leon.lee@gmail.com>
2021-02-07 19:52:28 +0000

    adding graphs babyyy

--- graphs.py
+++ graphs.py
@@ -12,31 +12,45 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+# Initialise dataframe
 df = pd.read_csv("Data/sortedData6M.csv")
-
-df["Date"] = pd.to_datetime(df["Date"], dayfirst="True")
+df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
 df["Time"] = df["Time"].str[0:5]
-#df["Time"] = pd.to_datetime(("21/12/2016 " + df["Time"]), format="%d/%m/%Y %H:%M:%S")
+print(df.loc[0,:])
 
-
+# Number of texts sent per day per person
 temp = df.groupby(["Date", "Name"]).size().reset_index(name="Count")
 data.append(temp)
-#graph1 = px.line(data1, x="Date", y="Count", color="Name")
 graphs["textsPerDay"] = px.line(data[0], x="Date", y="Count", color="Name")
 
-temp = df
+# Number of texts sent per day (Total)
+graphs["totalTextsPerDay"] = px.bar(data[0], x="Date", y="Count", color="Name", title="Chart")
+# graphs["totalTextsPerDay"].show()
 
 
+#temp = temp.set_index("Date")
+temp = temp.pivot(index="Date", columns="Name", values="Count")
+temp = temp.resample('M').mean()
+print(temp)
+data.append(temp)
+graphs["textsPerMonth"] = px.bar(data[1], x=data[1].index, y=["Kristi", "Leon"])
+graphs["textsPerMonth"].show()
+
+# Time message was sent
 temp = df.groupby(["Time"]).size().reset_index(name="Count")
 data.append(temp)
+graphs["timeOfText"] = px.bar(data[2], x="Time", y="Count")
 
-graphs["timeOfText"] = px.bar(data[1], x="Time", y="Count")
+# Percentages of messages sent
+temp = df.groupby(["Name"]).size().reset_index(name="Count")
+data.append(temp)
+graphs["percentMessages"] = px.pie(data[3], values="Count", names="Name")
 
 graphs["timeOfText"].update_layout(
    bargap=0
 )
 
-graphs["timeOfText"].show()
+#graphs["timeOfText"].show()
 
 
 app.layout = html.Div(children=[
@@ -49,10 +63,17 @@ app.layout = html.Div(children=[
       figure = graphs["textsPerDay"]
    ),
 
-   html.H3(children="Time message was sent"),
+   html.H3(children="Total texts per day"),
 
    dcc.Graph(
       id="graph2",
+      figure = graphs["totalTextsPerDay"]
+   ),
+
+   html.H3(children="Time message was sent"),
+
+   dcc.Graph(
+      id="graph3",
       figure = graphs["timeOfText"]
    ),
 ])
