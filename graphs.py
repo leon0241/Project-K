57f9049ec7bb981b4fc5e686b774aaeb260ded4a
Leon0241 <enliang.leon.lee@gmail.com>
2021-02-09 03:45:59 +0000

    words but emojis are fked

--- graphs.py
+++ graphs.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 import os
+import numpy as np
 
 graphs = {}
 data = []
@@ -21,12 +22,12 @@ def initialise_dataframe():
 
 # Number of texts sent per day per person
 def texts_over_date(df):
-   temp = df.groupby(["Date", "Name"]).size().reset_index(name="Count")
-   temp = temp.pivot(index="Date", columns="Name", values="Count")
-   temp["Total"] = temp["Kristi"] + temp["Leon"]
-   data.append(temp)
+   ndf = df.groupby(["Date", "Name"]).size().reset_index(name="Count")
+   ndf = ndf.pivot(index="Date", columns="Name", values="Count")
+   ndf["Total"] = ndf["Kristi"] + ndf["Leon"]
+   data.append(ndf)
    texts_per_day()
-   texts_per_month(temp)
+   texts_per_month(ndf)
 
 # Number of texts sent per day (Total)
 def texts_per_day():
@@ -36,48 +37,78 @@ def texts_per_day():
    graphs["totalTextsPerDay"] = px.bar(df, x=df.index, y=["Kristi", "Leon"], title="Chart")
 
 # Number of texts sent per month
-def texts_per_month(temp):
-   temp = temp.resample('M').mean()
-   data.append(temp)
+def texts_per_month(df):
+   df = df.resample('M').mean()
+   data.append(df)
    graphs["textsPerMonth"] = px.bar(data[1], x=data[1].index, y=["Kristi", "Leon"], barmode="group")
 
 # Day message sent
 def texts_day_of_week(df):
    # If you don't do this, df will get reassigned with the values of temp
-   temp = df.rename(columns = {"Name":"a"})
-   temp = temp.rename(columns = {"a":"Name"})
+   ndf = df.rename(columns = {"Name":"a"})
+   ndf = ndf.rename(columns = {"a":"Name"})
 
-   temp["Date"] = temp["Date"].dt.day_name()
-   temp = temp.groupby(["Date", "Name"]).size().reset_index(name="Count")
-   temp["Date"] = pd.Categorical(temp["Date"], categories=["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], ordered=True)
-   temp = temp.pivot(index="Date", columns="Name", values="Count")
-   data.append(temp)
+   ndf["Date"] = ndf["Date"].dt.day_name()
+   ndf = ndf.groupby(["Date", "Name"]).size().reset_index(name="Count")
+   ndf["Date"] = pd.Categorical(ndf["Date"], categories=["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], ordered=True)
+   ndf = ndf.pivot(index="Date", columns="Name", values="Count")
+   data.append(ndf)
    graphs["weekDaySent"] = px.bar(data[2], x=data[2].index, y=["Kristi", "Leon"], barmode="group")
 
 # Time message was sent
 def texts_per_time(df):
-   temp = df.groupby(["Time"]).size().reset_index(name="Count")
-   data.append(temp)
+   ndf = df.groupby(["Time"]).size().reset_index(name="Count")
+   data.append(ndf)
    graphs["timeOfText"] = px.bar(data[3], x="Time", y="Count")
 
 # Percentages of messages sent
 def text_ratio(df):
-   temp = df.groupby(["Name"]).size().reset_index(name="Count")
-   data.append(temp)
+   ndf = df.groupby(["Name"]).size().reset_index(name="Count")
+   data.append(ndf)
    graphs["percentMessages"] = px.pie(data[4], values="Count", names="Name")
 
 # Count words sent
-def count_words_sent(df):
-   temp = df
-   temp_day = data[0]
-   temp["Count"] = df["Contents"].str.split().str.len()
-   wordCount = temp["Count"].sum()
-   mostWords = temp_day["Total"].max()
-   avgMsgsK = temp_day["Kristi"].mean()
-   avgMsgsL = temp_day["Leon"].mean()
-   print(df)
-   print(temp_day)
-   print(wordCount, mostWords, avgMsgsK, avgMsgsL)
+def stat_functions(df):
+   ndf = df.rename(columns = {"Name":"a"})
+   ndf = ndf.rename(columns = {"a":"Name"})
+
+   ndf_d = data[0]
+   ndf["Count"] = df["Contents"].str.split().str.len()
+   messageCount = ndf.size
+   wordCount = ndf["Count"].sum()
+   pictureCount = ndf["Attachments"].count()
+
+   mostWords = ndf_d["Total"].max()
+   mostWordsDate = ndf_d["Total"].idxmax()
+   avgMsgs = ndf_d["Total"].mean()
+   avgMsgsLen = ndf["Count"].mean()
+
+   print(messageCount, wordCount, pictureCount, mostWords, mostWordsDate, avgMsgs, avgMsgsLen)
+
+def word_occurances(df):
+   ndf = df.rename(columns = {"Name":"a"})
+   ndf = ndf.rename(columns = {"a":"Name"})
+   ndf_k, ndf_l = [x for _, x in ndf.groupby(ndf["Name"] == "Leon")]
+
+   ndf_k = ndf_k.reset_index()
+   ndf_l = ndf_l.reset_index()
+   arr = [ndf_k, ndf_l]
+   for i in range(2):
+      ndf_t = arr[i]
+
+      words = ndf_t["Contents"].str.split(expand=True).stack().value_counts()
+      ndf_wt = words.to_frame()
+      ndf_wt = ndf_wt.reset_index()
+      ndf_wt = ndf_wt.rename(columns = {"index":"Word", 0:"Count"})
+
+      if i == 0:
+         ndf_wk = ndf_wt
+      else:
+         ndf_wl = ndf_wt
+   
+   ndf_wk.to_csv("Data/wordsL.csv")
+   ndf_wl.to_csv("Data/wordsK.csv")
+
 
 def layout():
    app.layout = html.Div(children=[
@@ -132,7 +163,8 @@ def main():
    texts_day_of_week(dataframe)
    texts_per_time(dataframe)
    text_ratio(dataframe)
-   count_words_sent(dataframe)
+   stat_functions(dataframe)
+   word_occurances(dataframe)
    layout()
 
 if __name__ == "__main__":
